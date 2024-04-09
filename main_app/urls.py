1022bb25e9a87a64fd90528f6bfeaf911b98403b
Luis Payan <luis.payan1200@gmail.com>
2024-04-09 15:11:22 -0400

    removed geolocation, and any reference from geolocation

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Home, CreateUserView, LoginView, VerifyUserView, ProfileDetail, FeedRun,FollowerRunFeed, CreateRun
+from .views import Home, CreateUserView, LoginView, VerifyUserView, ProfileDetail, FeedRun,FollowerRunFeed, CreateRun, UserRuns
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
@@ -9,9 +9,9 @@ urlpatterns = [
     path('profile/<int:user_id>/', ProfileDetail.as_view(), name='profile-detail'),
 
     #Run Paths:
-    path('runs/feed/', FeedRun.as_view(),name="feed-run")
+    path('runs/feed/', FeedRun.as_view(),name="feed-run"),
     path('runs/feed/followed/', FollowerRunFeed.as_view(), name='followed-run-feed'),
-    path('runs/:id/', UserRuns.as_view(), name='user-runs')
+    path('runs/:id/', UserRuns.as_view(), name='user-runs'),
 
     path('profile/<int:user_id>/run/', CreateRun.as_view(), name="create-run"),
 
